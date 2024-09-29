dffe4f2199330581834eb5c0f041d1bb487816b4
Abel Mesele <meseleabel99@gmail.com>
2024-09-29 05:48:47 +0300

    Work on chatbox content

--- src/app/ui/Chatbox.tsx
+++ src/app/ui/Chatbox.tsx
@@ -9,11 +9,27 @@ interface ChatboxProps {
 
 const Chatbox: React.FC<ChatboxProps> = ({ messages, onUpdateMessage, onSelectVersion }) => {
     return (
-        <div className="py-32 px-20 w-full">
-            {messages.map((msg) => (
-                <Message key={msg.id} message={msg} onUpdateMessage={onUpdateMessage} onSelectVersion={onSelectVersion} />
-            ))}
-        </div>
+        <>
+            {
+                messages.length !== 0 ?
+                    (<div className="py-32 px-20 w-full">
+                        {messages.map((msg) => (
+                            <Message key={msg.id} message={msg} onUpdateMessage={onUpdateMessage} onSelectVersion={onSelectVersion} />
+                        ))}
+                    </div>) : (
+                        <div className="hero bg-inherit min-h-screen">
+                            <div className="hero-content text-center">
+                                <div className="max-w-md">
+                                    <h1 className="text-5xl font-bold">Hello 👋🏾</h1>
+                                    <p className="py-6">
+                                        This is a simplified ChatGPT clone web application built using NextJS and Supabase. To get started, type in the input field below.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+            }
+        </>
     );
 };
 
